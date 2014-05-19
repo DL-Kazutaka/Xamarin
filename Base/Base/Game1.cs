93e5bae649679d65e2875edd8f1f5f17f1d7e8b7
kazutaka.matsusaki <kazutaka.matsusaki@dartslive.com>
2014-05-19 21:09:04 +0900

    iOS用のプロジェクトを追加

--- Base/Base/Game1.cs
+++ Base/Base/Game1.cs
@@ -1,52 +1,75 @@
-﻿#region Using Statements
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// BaseGame.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
 
 #endregion
 namespace Base
 {
 	/// <summary>
-	/// This is the main type for your game
+	/// Default Project Template
 	/// </summary>
 	public class Game1 : Game
 	{
+		#region Fields
+
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
+		Texture2D logoTexture;
+
+		#endregion
+
+		#region Initialization
 
 		public Game1 ()
 		{
+
 			graphics = new GraphicsDeviceManager (this);
-			Content.RootDirectory = "Content";	            
-			graphics.IsFullScreen = true;		
+			
+			Content.RootDirectory = "Content";
+
+			graphics.IsFullScreen = true;
 		}
 
 		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
+		/// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
+		/// we'll use the viewport to initialize some values.
 		/// </summary>
 		protected override void Initialize ()
 		{
-			// TODO: Add your initialization logic here
 			base.Initialize ();
-				
 		}
 
 		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
+		/// Load your graphics content.
 		/// </summary>
 		protected override void LoadContent ()
 		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch (GraphicsDevice);
-
-			//TODO: use this.Content to load your game content here 
+			// Create a new SpriteBatch, which can be use to draw textures.
+			spriteBatch = new SpriteBatch (graphics.GraphicsDevice);
+			
+			// TODO: use this.Content to load your game content here eg.
+			logoTexture = Content.Load<Texture2D> ("logo");
 		}
+
+		#endregion
+
+		#region Update and Draw
 
 		/// <summary>
 		/// Allows the game to run logic such as updating the world,
@@ -55,26 +78,36 @@ namespace Base
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update (GameTime gameTime)
 		{
+			// TODO: Add your update logic here
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
 				Exit ();
 			}
-			// TODO: Add your update logic here			
+
+            		
 			base.Update (gameTime);
 		}
 
 		/// <summary>
-		/// This is called when the game should draw itself.
+		/// This is called when the game should draw itself. 
 		/// </summary>
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw (GameTime gameTime)
 		{
+			// Clear the backbuffer
 			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
-		
+
+			spriteBatch.Begin ();
+
+			// draw the logo
+			spriteBatch.Draw (logoTexture, new Vector2 (130, 200), Color.White);
+
+			spriteBatch.End ();
+
 			//TODO: Add your drawing code here
-            
 			base.Draw (gameTime);
 		}
+
+		#endregion
 	}
 }
-
