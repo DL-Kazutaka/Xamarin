0b3a6606b40e48c85faaf5870e72281ec3d36e04
Kazutaka Matsusaki <kazutaka.matsusaki@dartslive.com>
2014-06-19 20:38:54 +0900

    ローカライズ用のプロジェクト追加

---
+++ Localization/Base/Game1.cs
@@ -0,0 +1,122 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// BaseGame.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+#endregion
+namespace Base
+{
+	/// <summary>
+	/// Default Project Template
+	/// </summary>
+	public class Game1 : Game
+	{
+		#region Fields
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D logoTexture;
+        SpriteFont font;
+
+		#endregion
+
+		#region Initialization
+
+		public Game1 ()
+		{
+
+			graphics = new GraphicsDeviceManager (this);
+			
+			Content.RootDirectory = "Content";
+#if !WINDOWS
+			graphics.IsFullScreen = true;
+#endif
+		}
+
+		/// <summary>
+		/// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
+		/// we'll use the viewport to initialize some values.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			base.Initialize ();
+		}
+
+		/// <summary>
+		/// Load your graphics content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be use to draw textures.
+			spriteBatch = new SpriteBatch (graphics.GraphicsDevice);
+			
+			// TODO: use this.Content to load your game content here eg.
+			logoTexture = Content.Load<Texture2D> ("Textures/logo");
+
+            font = Content.Load<SpriteFont>("Fonts/SpriteFont");
+		}
+
+		#endregion
+
+		#region Update and Draw
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			// TODO: Add your update logic here
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
+				Exit ();
+			}
+
+            		
+			base.Update (gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself. 
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			// Clear the backbuffer
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+			spriteBatch.Begin ();
+
+			// draw the logo
+            if (logoTexture != null)
+    			spriteBatch.Draw (logoTexture, new Vector2 (130, 200), Color.White);
+
+            if (font != null)
+            {
+                spriteBatch.DrawString(font, Resources.values.Strings.Test, new Vector2(100, 100), Color.White);
+            }
+			spriteBatch.End ();
+
+			//TODO: Add your drawing code here
+			base.Draw (gameTime);
+		}
+
+		#endregion
+	}
+}
---
+++ Web/Base/Game1.cs
@@ -0,0 +1,136 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// BaseGame.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+using Android.Webkit;
+using Android.Content;
+#endregion
+namespace Base
+{
+	/// <summary>
+	/// Default Project Template
+	/// </summary>
+	public class Game1 : Game
+	{
+		#region Fields
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D logoTexture;
+
+		#endregion
+
+		#region Initialization
+
+		public Game1 ()
+		{
+
+			graphics = new GraphicsDeviceManager (this);
+			
+			Content.RootDirectory = "Content";
+#if !WINDOWS
+			graphics.IsFullScreen = true;
+#endif
+		}
+
+		/// <summary>
+		/// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
+		/// we'll use the viewport to initialize some values.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			base.Initialize ();
+
+            TouchPanel.EnabledGestures = GestureType.Tap;
+		}
+
+		/// <summary>
+		/// Load your graphics content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be use to draw textures.
+			spriteBatch = new SpriteBatch (graphics.GraphicsDevice);
+			
+			// TODO: use this.Content to load your game content here eg.
+			logoTexture = Content.Load<Texture2D> ("logo");
+		}
+
+		#endregion
+
+		#region Update and Draw
+
+        WebView web_view;
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			// TODO: Add your update logic here
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
+				Exit ();
+			}
+
+            while (TouchPanel.IsGestureAvailable)
+            {
+                // read the next gesture from the queue
+                GestureSample gesture = TouchPanel.ReadGesture();
+
+                // we can use the type of gesture to determine our behavior
+                switch (gesture.GestureType)
+                {
+                    // on taps, we change the color of the selected sprite
+                    case GestureType.Tap:
+                        Android.Net.Uri uri = Android.Net.Uri.Parse("https://www.google.co.jp/");
+                        Intent intent = new Intent(Intent.ActionView, uri);
+                        Window.Context.StartActivity(intent);
+                        break;
+                }
+            }
+            		
+			base.Update (gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself. 
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			// Clear the backbuffer
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+			spriteBatch.Begin ();
+
+			// draw the logo
+            if (logoTexture != null)
+    			spriteBatch.Draw (logoTexture, new Vector2 (130, 200), Color.White);
+
+			spriteBatch.End ();
+
+			//TODO: Add your drawing code here
+			base.Draw (gameTime);
+		}
+
+		#endregion
+	}
+}
